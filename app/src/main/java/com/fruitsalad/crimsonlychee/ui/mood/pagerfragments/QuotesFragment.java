c6b49799b255f8a66cc39fb83103f84461338a1d
AbhilashG97 <abhilash.ag1997@gmail.com>
2020-01-26 10:55:17 +0530

    initial project state

---
+++ app/src/main/java/com/fruitsalad/crimsonlychee/ui/mood/pagerfragments/QuotesFragment.java
@@ -0,0 +1,32 @@
+package com.fruitsalad.crimsonlychee.ui.mood.pagerfragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.fruitsalad.crimsonlychee.R;
+
+
+public class QuotesFragment extends Fragment {
+
+    public QuotesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quotes, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/fruitsalad/crimsonlychee/ui/mood/pagerfragments/VideosFragment.java
@@ -0,0 +1,26 @@
+package com.fruitsalad.crimsonlychee.ui.mood.pagerfragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.fruitsalad.crimsonlychee.R;
+
+public class VideosFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vidoes, container, false);
+    }
+
+}
